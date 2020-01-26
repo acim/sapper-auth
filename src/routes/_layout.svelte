1f10ff155884639ce4b4e6ec85fccf3ae89ff628
Boban Acimovic <boban.acimovic@gmail.com>
2020-01-26 12:37:06 +0100

    fix empty block

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,11 +1,19 @@
 <script>
-	import Nav from '../components/Nav.svelte';
+  import Nav from "../components/Nav.svelte";
 
-	export let segment;
+  export let segment;
 </script>
 
-<Nav {segment}/>
+<Nav {segment} />
 
 <main>
-	<slot></slot>
+  <slot />
 </main>
+
+<sapper:base>
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css"
+    integrity="sha256-D9M5yrVDqFlla7nlELDaYZIpXfFWDytQtiV+TaH6F1I="
+    crossorigin="anonymous" />
+</sapper:base>
